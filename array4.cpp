0d21000350af4819dd28d25c61a29f6ab56cf4bd
Mohitku-Panigrahi <mohitkumarpanigrahi533@gmail.com>
2026-09-17 10:33:53 +0530

    random commit3

--- array4.cpp
+++ array4.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
     int arr[10];//sized element
-    
+    // int arr1[];//canaot declare like this we need to initialise something
+    int arr1[]={1,2,3,4};//like this
 }
