1b436e55a32ed64ee9ec68fb63c4c26da20ea66c
Mohitku-Panigrahi <mohitkumarpanigrahi533@gmail.com>
2026-09-17 10:40:44 +0530

    random commit5

--- array4.cpp
+++ array4.cpp
@@ -5,6 +5,9 @@ int main(){
     // int arr1[];//canaot declare like this we need to initialise something
     int arr1[]={1,2,3,4};//like this
     for(int i=0;i<10;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<10;i++){
         cout<<"arr ["<<i<<"] : "<<arr[i]<<endl;
     }
 }
