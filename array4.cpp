b35fbb006252db842197fcaf04fd8076499c3a1c
Mohitku-Panigrahi <mohitkumarpanigrahi533@gmail.com>
2026-09-17 10:55:12 +0530

    random commit6

--- array4.cpp
+++ array4.cpp
@@ -4,10 +4,15 @@ int main(){
     int arr[10];//sized element
     // int arr1[];//canaot declare like this we need to initialise something
     int arr1[]={1,2,3,4};//like this
+    int sum=0;
     for(int i=0;i<10;i++){
         cin>>arr[i];
     }
     for(int i=0;i<10;i++){
         cout<<"arr ["<<i<<"] : "<<arr[i]<<endl;
+    }
+    for(int i=0;i<10;i++){
+        sum+=arr[i];
+        cout<<"arr ["<<i<<"] : "<<sum<<endl;
     }
 }
