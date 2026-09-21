2d15f513ed69ddcaf678d1db1f1aa600549732a6
Mohitku-Panigrahi <mohitkumarpanigrahi533@gmail.com>
2026-09-21 09:39:54 +0530

    random commi565

--- constructor.cpp
+++ constructor.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 using namespace std;
+class student
+{
+private:
+    int rollNo, regdNum;
+    string name,section;
+public:    
+    student(){
+        cout<<"Enter roll, regd"<<endl;
+        cin>>rollNo>>regdNum;
+        cout<<"Enter name and section"<<endl;
+        cin>>name>>section;
+    }
+    void display(){
+        cout<<"Entered value "<<endl;
+        cout<<rollNo<<endl;
+        cout<<regdNum<<endl;
+        cout<<name<<endl;
+        cout<<section<<endl;
+    }
+};
 int main(){
     
 }
